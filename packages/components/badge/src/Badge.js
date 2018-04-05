312fcbcb5724e4f10f1d5050ab2b696a319adf89
Doug Miller <dmiller9911@gmail.com>
2018-04-05 16:15:50 -0400

    fix(button & badge): removed console logs to force publish
    affects: @dougmiller/badge-lerna, @dougmiller/button-lerna

--- packages/components/badge/src/Badge.js
+++ packages/components/badge/src/Badge.js
@@ -1,2 +1,1 @@
 export { Badge as default } from 'react-bootstrap';
-console.log('Testing Publish and verions');
--- packages/components/button/src/Button.js
+++ packages/components/button/src/Button.js
@@ -1,2 +1,1 @@
 export { Button as default } from 'react-bootstrap';
-console.log('Testing Publish and verions');
